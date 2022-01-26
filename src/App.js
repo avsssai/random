00eac8c72918060609d1db064dd9b1ff9d6dbfbe
avsssai <avsssai@gmail.com>
2022-01-27 02:41:16 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { words, colors, backgrounds } from "./words";
+import TextTransition, { presets } from "react-text-transition";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [index, setIndex] = useState(0);
+	// const [color, setColor] = useState("black");
+	// useEffect(() => {
+
+	// }, []);
+	useEffect(() => {
+		// const colorToApply = () => colors[Math.floor(Math.random() * colors.length)];
+		// setColor(colorToApply());
+		const intervalId = setInterval(() => setIndex((index) => index + 1), 500);
+		return () => clearTimeout(intervalId);
+	}, []);
+	const backgroundToApply = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+	const colorToApply = () => colors[Math.floor(Math.random() * colors.length)];
+
+	return (
+		<div
+			style={{
+				background: `#${backgroundToApply}`,
+				height: "100vh",
+				width: "100vw",
+				alignItems: "center",
+				display: "flex",
+				justifyContent: "center",
+			}}>
+			<h1>
+				Sruthi is{" "}
+				<span style={{ display: "inline", color: `#${colorToApply()}` }}>
+					<TextTransition
+						text={words[index % words.length]}
+						springConfig={presets.wobbly}
+						style={{ display: "inline" }}
+					/>
+				</span>
+			</h1>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,9 @@
+const BREAKPOINTS = {
+	tabletMin: 550,
+	laptopMin: 1100,
+	desktopMin: 1500,
+};
+
+// const QUERIES = {
+//     tabletAndUp:
+// }
